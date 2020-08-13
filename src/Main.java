814d4a7cb99fe365a9a1e9e3771fe7f4362003e7
name <email>
2020-08-13 17:08:32 +0900

    ver2

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,60 @@
-import java.util.Scanner; // scanner 사용하기 위한 참조값
-import java.util.*; // arraylist 사용하기 위한 참조값
+import java.util.Scanner; // scanner를 사용하기 위함
+import java.util.*; // arraylist를 사용하기 위함
 
 class Main {
   public static void main(String[] args) {
+	  // 입력받는 함수
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> Titles = new ArrayList<>();
+    ArrayList<String> Bodies = new ArrayList<>();
+    String title;
+    String body;
+    int cnt = 0;
+    // print 클래스 호출
+    print p = new print();
     
-	  Scanner sc = new Scanner(System.in);
-	  // 배열 대신 arraylist 사용
-	  ArrayList<String> Titles = new ArrayList<>();
-	  ArrayList<String> Bodies = new ArrayList<>();
-	  String title;
-	  String body;
-	  int cnt = 0;
-	  
-	  while(true) {
-		  String s = sc.nextLine();
-		  
-		  // 입력값이 exit인지 아닌지 확인 후 exit일 시 프로그램 종료
-		  if(s.equals("exit")) {
-			  System.out.println("프로그램을 종료합니다.");
-			  break;
-		  }
-		  // 입력값이 add인지 아닌지 확인후 add일 시 게시물 추가
-		  else if(s.equals("add")) {
-			  System.out.println("제목을 입력하시오.");
-			  title = sc.nextLine();
-			  // arraylist에 입력값 저장
-			  Titles.add(title);
-			  System.out.println("내용을 입력하시오.");
-			  body = sc.nextLine();
-			  // arraylist에 입력값 저장
-			  Bodies.add(body);
-			  cnt++;
-		  }
-		  // 입력값이 list인지 아닌지 확인 후 list일 시 게시물 조회
-		  else if(s.equals("list")) {
-			  for(int i = 0; i < cnt; i++) {
-				  System.out.println("제목 : ");
-				  // arraylist에 저장된 입력값 불러오기
-				  System.out.println(Titles.get(i));
-				  System.out.println("내용 : ");
-				  // arraylist에 저장된 입력값 불러오기
-				  System.out.println(Bodies.get(i));
-				  System.out.println("");
-			  }
-		  }
-	  }
+    while(true) {
+    	// print 클래스 안의 menu 메소드 호출
+		System.out.println("-------------------------");
+    	p.menu();
+		System.out.println("-------------------------");
+    	String s = sc.nextLine();
+    	if(s.equals("exit")) {
+    		System.out.println("프로그램을 종료합니다.");
+    		break;
+    	}
+    	else if(s.equals("add")) {
+    		System.out.println("제목을 입력하시오.");
+    		title = sc.nextLine();
+    		// title에 입력된 값을 Titles에 저장
+    		Titles.add(title);
+    		System.out.println("내용을 입력하시오.");
+    		body = sc.nextLine();
+    		// body에 입력된 값을 Bodies에 저장
+    		Bodies.add(body);
+    		cnt++;
+    	}
+    	else if(s.equals("list")) {
+    		for(int i = 0; i < cnt; i++) {
+    			System.out.println("");
+    			System.out.println((i + 1) + "번째");
+    			// Titles에 저장된 값을 불러오는 함수(get())
+    			System.out.println("제목 : " + Titles.get(i));
+    			// Bodies에 저장된 값을 불러오는 함수(get())
+    			System.out.println("내용 : " + Bodies.get(i));
+    		}
+			System.out.println("");
+    	}
+    }
     
   }
 }
 
 class print {
-	
+	void menu() {
+		System.out.println("원하는 기능에 맞게 입력해주십시오.");
+		System.out.println("게시물 추가 : add");
+		System.out.println("게시물 조회 : list");
+		System.out.println("프로그램 종료 : exit");
+	}
 }
